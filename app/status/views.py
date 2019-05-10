475961509be402c14e6c2be3f732521523cc90fa
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2019-05-10 10:27:45 +0100

    replace status endpoint with extremely simplified version

--- app/status/views.py
+++ app/status/views.py
@@ -7,6 +7,4 @@ from dmutils.status import get_app_status
 
 @status.route('/_status')
 def status():
-    return get_app_status(data_api_client=data_api_client,
-                          search_api_client=None,
-                          ignore_dependencies='ignore-dependencies' in request.args)
+    return '{"status":"ok"}', 200
